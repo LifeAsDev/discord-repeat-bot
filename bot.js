f685408392e9d96e11faa02af3d5920e826bbd73
LifeAsDev <angelocraft551@gmail.com>
2025-10-11 11:24:39 -0400

    filesv3

--- bot.js
+++ bot.js
@@ -13,7 +13,7 @@ app.use(express.urlencoded({ extended: true }));
 
 const publicPath = path.join(__dirname, "public");
 
-const versionPath = "/filesv2"; // 🔹 cambia esto a /filesv2, /filesv3 cuando actualices
+const versionPath = "/filesv3"; // 🔹 cambia esto a /filesv2, /filesv3 cuando actualices
 
 app.use(
 	versionPath,
