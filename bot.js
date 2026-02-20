8627e1683258d4278898db4905d55e52af6d6a96
LifeAsDev <angelocraft551@gmail.com>
2026-02-20 18:41:04 -0400

    v56

--- bot.js
+++ bot.js
@@ -18,7 +18,7 @@ app.use(express.urlencoded({ extended: true }));
 
 const publicPath = path.join(__dirname, "public");
 
-const versionFile = 55;
+const versionFile = 56;
 
 app.use(
 	express.static(publicPath, {
